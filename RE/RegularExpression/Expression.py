6c0834ee41e6f062a1843f4f344d3adcec1cefcd
000alen <lclc.alen@gmail.com>
2020-07-13 13:52:21 -0400

    Add match_all, search_all and split

--- RE/RegularExpression/Expression.py
+++ RE/RegularExpression/Expression.py
@@ -1,20 +1,10 @@
-from typing import List, Tuple
+from typing import List, Tuple, Iterator
 
 from RE.FiniteStateMachine import FiniteStateMachine
 
 __all__ = (
-    "DIGITS",
-    "LETTERS_LOWER",
-    "LETTERS_UPPER",
-    "LETTERS",
     "Expression"
 )
-
-DIGITS = frozenset("0123456789")
-
-LETTERS_LOWER = frozenset("abcdefghijklmnopqrstuvwxyz")
-LETTERS_UPPER = frozenset("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-LETTERS = LETTERS_LOWER | LETTERS_UPPER
 
 
 class Expression:
@@ -81,7 +71,23 @@ class Expression:
             if current_states & self.finite_state_machine.final_states:
                 last_final_position = position
         if last_final_position is not None:
-            return string[start: start + last_final_position]
+            return string[start: start + last_final_position + 1]
+
+    def match_all(self, string: str, start: int = 0, end: int = None) -> Iterator[str]:
+        assert string
+        if self.finite_state_machine is None:
+            self.compile()
+        if end is None:
+            end = len(string)
+        while start < end:
+            last_final_position = None
+            for position, element, current_states in self.finite_state_machine.run(string[start:end]):
+                if current_states & self.finite_state_machine.final_states:
+                    last_final_position = position
+            if last_final_position is not None:
+                yield string[start: start + last_final_position + 1]
+                start += last_final_position
+            start += 1
 
     def search(self, string: str, start: int = 0, end: int = None) -> Tuple[int, str]:
         assert string
@@ -94,8 +100,29 @@ class Expression:
             if match:
                 return i, match
 
-    def split(self):
-        raise NotImplementedError
+    def search_all(self, string: str, start: int = 0, end: int = None) -> Iterator[Tuple[int, str]]:
+        assert string
+        if self.finite_state_machine is None:
+            self.compile()
+        if end is None:
+            end = len(string)
+        for i in range(start, end):
+            match = self.match(string[i:end])
+            if match:
+                yield i, match
+
+    def split(self, string: str, start: int = 0, end: int = None) -> Tuple[str]:
+        assert string
+        if self.finite_state_machine is None:
+            self.compile()
+        if end is None:
+            end = len(string)
+        new_string = []
+        position = start
+        for i, match in self.search_all(string[start:end]):
+            new_string.append(string[position:i])
+            position = i + 1
+        return tuple(new_string)
 
     def build(
             self,
--- main.py
+++ main.py
@@ -1,23 +1,5 @@
-"""A very basic definition of BaseTypes using RE."""
-
 from RE.RegularExpression.Literal import Literal
-from RE.RegularExpression.One import One
-from RE.RegularExpression.Optional import Optional
-from RE.RegularExpression.Wildcard import Wildcard
-from RE.RegularExpression.Zero import Zero
 
-_sign = Literal("+") | Literal("-")
-_digit = Literal("0") >> Literal("9")
-
-base_types = {
-    "integer": Optional(_sign) + One(_digit),
-    "decimal": Optional(_sign) + Zero(_digit) + Literal(".") + One(_digit),
-    "boolean": Literal("True") | Literal("False"),
-    "undefined": Literal("Undefined"),
-    "string": Literal("\"") + Zero(Wildcard()) + Literal("\""),
-}
-
+expression = Literal(", ")
 string = input("> ")
-for name, expression in base_types.items():
-    print(name.upper())
-    print(expression.search(string), end="\n\n")
+print(expression.split(string))
